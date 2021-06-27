f55064f10ba801ba83af63734bc5007b154b2c90
Mikhail Pakhomov <mpakhomov99@gmail.com>
2021-06-27 20:49:26 +0300

    solved problem with obstacle disappearing

--- core/src/com/mygdx/game/Barrier.java
+++ core/src/com/mygdx/game/Barrier.java
@@ -8,16 +8,52 @@ public class Barrier {
 
     class BarrierImage {
 
-        private Texture tx;
+        private int speed;
         private Vector2 pos;
 
         public BarrierImage(Vector2 pos) {
-            this.tx = new Texture("cactus_resized.png");
+            this.speed = 4;
             this.pos = pos;
+        }
+
+        public void update() {
+            pos.x -= speed;
+
+            if (pos.x < -200) {
+                pos.x = 1280;
+            }
         }
 
     }
 
+    private BarrierImage[] barriers;
+    private Texture tx;
+    //private int betweenDistance;
+
+    public Barrier() {
+        tx = new Texture("cactus_resized.png");
+        barriers = new BarrierImage[3];
+        int startPos = 500;
+        //betweenDistance = 500;
+
+        for (int i = 0; i < barriers.length; i++) {
+            barriers[i] = new BarrierImage(new Vector2(startPos, 100));
+            startPos += 500;
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        for (BarrierImage barrier : barriers) {
+            batch.draw(tx, barrier.pos.x, barrier.pos.y);
+        }
+    }
+
+    public void update() {
+        for(BarrierImage barrier : barriers) {
+            barrier.update();
+        }
+
+    /*
     private BarrierImage[] barriers = new BarrierImage[4];
     private int speed;
 
@@ -45,5 +81,6 @@ public class Barrier {
                 barriers[i].pos.x = (i + 1) * 500;
             }
         }
+     */
     }
 }
